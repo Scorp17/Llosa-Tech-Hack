30b496f5284bcbca94a2581016fa5480354784c7
Scorp17 <josuellosadash@gmail.com>
2026-01-09 17:34:14 -0500

    Crear puente.c

---
+++ puente.c
@@ -0,0 +1,8 @@
+#include <stdlib.h>
+
+int main() {
+    // Reemplaza 'llosatech-hack' con el nombre real de tu script
+    // Si el script está en la misma carpeta, usa "./"
+    system("bash ./.llosatech-hack");
+    return 0;
+}
